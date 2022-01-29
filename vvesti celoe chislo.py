d832fd73e0f3fdf51abcf062e6fc76578ce262a4
Marina <98482958+MarinaVik@users.noreply.github.com>
2022-01-29 22:58:49 +0200

    Add files via upload

---
+++ vvesti celoe chislo.py
@@ -0,0 +1,3 @@
+a = int(input('Введите число в диапазоне от-1000 до +1000:'))
+print('The next number for the number:' , a , 'is' , a+1)
+print('The previous number for the number:' , a , 'is' , a-1)
