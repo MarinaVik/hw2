69a1b05e2099f07e716930861439951084d59aa6
Marina <98482958+MarinaVik@users.noreply.github.com>
2022-01-29 23:41:33 +0300

    Delete treugolnik.py

--- treugolnik.py
+++
@@ -1,9 +0,0 @@
-# найти площадь и периметр
-import math
-AB = float (input("длина первого катета:"))
-AC = float (input("длина второго катета:"))
-BC = math.sqrt(AB**2*AC**2)
-s = (AB*AC)/2
-p = AB + AC + BC
-print (s)
-print (p)
